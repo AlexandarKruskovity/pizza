3a5400ac7906aa30f2b2f4c2b83b5f80b9daef31
Kruskovity Alexandar <kruskovityalexandar@turr.hu>
2025-01-07 12:28:51 +0100

    osszeomlas,javitas

---
+++ index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const burgerBtn = document.querySelector('.js-menu-toggle-btn');
+  
+    const toggleMobileMenu = () => {
+      burgerBtn.classList.toggle('hamburger-menu--active');
+      /* you can add your mobile menu functionality here */
+    };
+    if (burgerBtn) {
+      burgerBtn.addEventListener('click', toggleMobileMenu);
+    }
+  });
